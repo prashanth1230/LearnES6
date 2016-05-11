ef2c24ac0ffddc41b6337dce6a23b0fd1ab565d7
Udupa, Prashanth <prashanth.udupa@fmr.com>
2016-05-11 15:57:41 +0530

    Added const example

---
+++ const.ts
@@ -0,0 +1,16 @@
+const a = 12; //accessible globally
+
+function myFunction() {
+    console.log(a);
+
+    const b = 13; //accessible throughout function
+
+    if (true) {
+        const c = 14; //accessible throughout the "if" statement
+        console.log(b);
+    }
+
+    //console.log(c); //Can't access c here
+}
+
+myFunction();
