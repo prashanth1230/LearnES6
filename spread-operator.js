6b98f8233c50959238ac214773fc7d0bfcf9a43b
Udupa, Prashanth <prashanth.udupa@fmr.com>
2016-05-12 13:34:59 +0530

    Added an example of array merging using spread operator.

--- spread-operator.js
+++ spread-operator.js
@@ -11,3 +11,9 @@ console.log(firstResult);
 let data = [2, 3];
 let secondResult = accpetsTwoParameters(...data);
 console.log(secondResult);
+
+//More examples of the spread operator
+let array1 = [1, 2, 4];
+let array2 = [23, 4, 6, 7];
+let array3 = [2, ...array1, 3, 67, ...array2];
+array3.forEach((e) => console.log(e));
